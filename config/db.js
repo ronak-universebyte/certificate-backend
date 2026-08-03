4453ee7f559d6e1d87d013e4f266ec73f0b432bc
Ronak <ronak.sksn@gmail.com>
2026-08-03 13:02:49 +0530

    Mongodb insert record fix

--- config/db.js
+++ config/db.js
@@ -1,18 +1,18 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("Mongo URI:", process.env.MONGODB_URI);
-
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log("✅ Connected");
+    console.log("✅ MongoDB Connected");
     console.log("Host:", conn.connection.host);
-    console.log("DB:", conn.connection.name);
+    console.log("Database:", conn.connection.name);
   } catch (err) {
-    console.error("Error Name:", err.name);
-    console.error("Error Message:", err.message);
-    console.error(err);
+    console.error("MongoDB Error:", err);
+    process.exit(1);
   }
 };
 
